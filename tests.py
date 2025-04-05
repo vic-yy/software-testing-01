50af0c592845ab61e6384b600a0fd1a78655610f
vic-dti <victor.yano@dtidigital.com.br>
2025-04-04 23:38:19 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -226,3 +226,79 @@ def test_select_mixed_choices(question):
 
     assert question.select_choices([2,3,4,5]) == [2,3]
     assert question.select_choices([1,2,3,4,5]) == [1,2,3]
+    
+def test_select_no_choices_returns_empty(question):
+    """Selecionar uma lista vazia deve retornar vazio"""
+    assert question.select_choices([]) == []
+
+def test_set_correct_choices_overwrites_previous(question):
+    """Definir novas escolhas corretas deve sobrescrever as antigas"""
+    question.max_selections = 5
+    question.set_correct_choices([1, 2])
+    assert set(question.select_choices([1, 2])) == {1, 2}
+
+    question.set_correct_choices([3])
+    assert question.select_choices([3]) == [3]
+
+
+def test_add_choice_increments_id_correctly_after_removal():
+    """Adicionar escolhas após remoção deve continuar incrementando id"""
+    question = Question(title='q1')
+    c1 = question.add_choice('a')
+    c2 = question.add_choice('b')
+    question.remove_choice_by_id(c1.id)
+    c3 = question.add_choice('c')
+
+    assert c2.id == 2
+    assert c3.id == 3
+
+def test_choice_text_is_stored_correctly():
+    """O texto da escolha deve ser armazenado corretamente"""
+    question = Question(title='q1')
+    choice = question.add_choice('Option Text')
+    assert choice.text == 'Option Text'
+
+def test_question_id_is_unique():
+    """Cada questão deve ter id único"""
+    question1 = Question(title='q1')
+    question2 = Question(title='q2')
+    assert question1.id != question2.id
+
+def test_cannot_remove_choice_twice():
+    """Remover a mesma escolha duas vezes deve dar erro"""
+    question = Question(title='q1')
+    choice = question.add_choice('a')
+    question.remove_choice_by_id(choice.id)
+
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(choice.id)
+
+def test_set_no_correct_choices_all_should_be_incorrect(question):
+    """Se não setar corretos, todos devem ser incorretos"""
+    question.max_selections = 5 
+    assert question.select_choices([1, 2, 3, 4, 5]) == []
+
+def test_select_invalid_choice_id_returns_empty_list(question):
+    """Selecionar um ID inválido deve retornar lista vazia"""
+    question.max_selections = 5
+    assert question.select_choices([999]) == []
+
+def test_remove_choice_does_not_affect_other_ids(question):
+    """Remover uma escolha não altera os ids das restantes"""
+    question = Question(title='q1')
+    question.add_choice('a')
+    question.add_choice('b')
+    question.add_choice('c')
+    question.remove_choice_by_id(2)
+
+    remaining_ids = [choice.id for choice in question.choices]
+    assert remaining_ids == [1, 3]
+
+def test_correct_choices_selection_with_duplicates(question):
+    """Selecionar IDs corretos duplicados retorna sem duplicar"""
+    question.max_selections = 5
+    question.set_correct_choices([1, 2])
+
+    selected = question.select_choices([1, 1, 2, 2])
+    
+    assert selected == [1, 1, 2, 2]
